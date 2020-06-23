bc67e938e2759733d6b504852eb7b00a9bd2c196
Tamara Nieves <tamaranieves93@gmail.com>
2020-06-23 10:09:24 +0200

    add functinality

--- js/app.js
+++ js/app.js
@@ -1,28 +1,20 @@
+const input = document.querySelector("#message");
+const message = document.querySelector(".message-content");
+const submit = document.querySelector("#submitBtn");
 
-(function() {
-//Select the input element
-const form = document.querySelector('#message-form')
-//Set up Submit Button
-form.addEventListener('submit', function(e){
-    // prevent the form's default submission action
-    e.preventDefault()
-    //Get user's input from from
-    const message = document.querySelector('#message')
-    const feedback = document.querySelector('.feedback')
-    const messageContent = document.querySelector('.message-content')
-
-    if (message.value === ''){
-        feedback.classList.add('show')
-        setTimeout(function(){
-        feedback.classList.remove('show')
-        }, 2000)
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (input.value === "") {
+        const alert = document.querySelector(".alert-danger");
+        alert.classList.add("show");
+        setTimeout(function() {
+            alert.classList.remove("show")
+        }, 2000);
     } else {
-        //Change message content and clear the message input
-        messageContent.textContent = message.value
-        message.value = ''
+        message.textContent = input.value;
+        input.value = "";
     }
 })
-})()
 
 
 
